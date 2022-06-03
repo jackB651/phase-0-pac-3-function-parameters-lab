709c97cfd0bba43bfbb334d23cb95393ce9381e1
Jonathon <jwbialas@gmail.com>
2022-06-03 00:37:52 -0500

    commit tree d75ccf47ba195ea315478e668470b61b22b8380d

---
+++ index.js
@@ -0,0 +1,15 @@
+x = "Aki"
+function introduction(name){
+   return `Hi, my name is ${name}.`;
+}
+console.log(introduction(x));
+
+function introductionWithLanguage(name,language){
+   return `Hi, my name is ${name} and I am learning to program in ${language}.`
+}
+console.log(introductionWithLanguage(x,Ember.js));
+
+function introductionWithLanguageOptional(name,language = "JavaScript"){
+   return `Hi, my name is ${name} and I am learning to program in ${language}.`
+}
+console.log(introductionWithLanguageOptional("Gracie","Python"));
